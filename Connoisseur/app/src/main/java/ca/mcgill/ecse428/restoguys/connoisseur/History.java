564304accb783111fc9d94d97cb6bbd299a83e27
Bernard <Bernard.kaminski@mail.mcgill.ca>
2016-01-27 17:13:38 -0500

    added way to get content from home screen

--- Connoisseur/app/src/main/java/ca/mcgill/ecse428/restoguys/connoisseur/History.java
+++ Connoisseur/app/src/main/java/ca/mcgill/ecse428/restoguys/connoisseur/History.java
@@ -1,15 +1,20 @@
 package ca.mcgill.ecse428.restoguys.connoisseur;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.ListView;
 
 public class History extends ActionBarActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Intent intent = getIntent();
+        ListView listView = (ListView) findViewById(R.id.listView);
     }
 
 }
