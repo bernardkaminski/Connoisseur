a1f26918aeabf1f4f049f640ef4f34d9c13b9227
Nicolas Velastegui <nvelastegui@outlook.com>
2016-01-28 16:35:44 -0500

    Added required activities with default XMLs

---
+++ Connoisseur/app/src/main/java/ca/mcgill/ecse428/restoguys/connoisseur/RestaurantDetails.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse428.restoguys.connoisseur;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class RestaurantDetails extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_restaurant_details);
+	}
+}
