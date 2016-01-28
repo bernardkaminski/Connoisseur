fe2adb63ac79ee66b08f35c31ca8ca419e6def91
Bernard <Bernard.kaminski@mail.mcgill.ca>
2016-01-27 22:12:24 -0500

    added code to display items and to respond to item click

--- Connoisseur/app/src/main/java/ca/mcgill/ecse428/restoguys/connoisseur/History.java
+++ Connoisseur/app/src/main/java/ca/mcgill/ecse428/restoguys/connoisseur/History.java
@@ -3,7 +3,10 @@ package ca.mcgill.ecse428.restoguys.connoisseur;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class History extends ActionBarActivity {
 
@@ -15,6 +18,17 @@ public class History extends ActionBarActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         Intent intent = getIntent();
         ListView listView = (ListView) findViewById(R.id.listView);
+        //have list here for now just to display something , later will be retrieved from db or file
+        String[] restaurantArray = {"Resto1","Resto2","Resto3"};
+        ArrayAdapter adapter = new ArrayAdapter<String>(this,  R.layout.activity_listview, restaurantArray);
+        listView.setAdapter(adapter);
+
+
+    }
+
+    public void openRestoinfo(View view) {
+        TextView t = (TextView) view;
+        CharSequence resto = t.getText();
     }
 
 }
