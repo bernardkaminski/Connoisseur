1ffc78e7e96525d28747ac6b966bd50dd4e96a24
Nicolas Velastegui <nvelastegui@outlook.com>
2016-02-11 00:23:10 -0500

    Added some random selection options to the dropdowns on the homescreen.

--- app/src/main/java/ca/mcgill/ecse428/restoguys/connoisseur/homescreen.java
+++ app/src/main/java/ca/mcgill/ecse428/restoguys/connoisseur/homescreen.java
@@ -8,6 +8,8 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -15,11 +17,53 @@ import android.view.View;
  */
 public class HomeScreen extends ActionBarActivity {
 
+	private Spinner spinnerOptionDistance;
+	private Spinner spinnerOptionCost;
+	private Spinner spinnerOptionRestaurantType;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.activity_homescreen);
+
+		populateSpinners();
+
+	}
+
+	/**
+	 * Populate homescreen spinners.
+	 */
+	private void populateSpinners () {
+
+		// Grab all the views
+
+		spinnerOptionDistance       = (Spinner) findViewById(R.id.activity_homescreen_spinner_Distance);
+		spinnerOptionCost           = (Spinner) findViewById(R.id.activity_homescreen_spinner_Cost);
+		spinnerOptionRestaurantType = (Spinner) findViewById(R.id.activity_homescreen_spinner_RestaurantType);
+
+		// Use default spinner adapters to populate each spinner with array from strings.xml
+
+		ArrayAdapter<CharSequence> adapterSpinnerDistance = ArrayAdapter.createFromResource(
+				this,
+				R.array.homescreen_array_options_distance, android.R.layout.simple_spinner_item
+		);
+		adapterSpinnerDistance.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spinnerOptionDistance.setAdapter(adapterSpinnerDistance);
+
+		ArrayAdapter<CharSequence> adapterSpinnerCost = ArrayAdapter.createFromResource(
+				this,
+				R.array.homescreen_array_options_food_cost, android.R.layout.simple_spinner_item
+		);
+		adapterSpinnerCost.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spinnerOptionCost.setAdapter(adapterSpinnerCost);
+
+		ArrayAdapter<CharSequence> adapterSpinnerRestaurantType = ArrayAdapter.createFromResource(
+				this,
+				R.array.homescreen_array_options_restaurant_type, android.R.layout.simple_spinner_item
+		);
+		adapterSpinnerRestaurantType.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spinnerOptionRestaurantType.setAdapter(adapterSpinnerRestaurantType);
 
 	}
 
