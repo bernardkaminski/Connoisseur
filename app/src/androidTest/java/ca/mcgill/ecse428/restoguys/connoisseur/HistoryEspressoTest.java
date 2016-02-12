82ea394c1f60f8c5aa78f0441f1f337fc84f2da4
Bernard <Bernard.kaminski@mail.mcgill.ca>
2016-02-11 21:11:23 -0500

    fixed last test case

--- app/src/androidTest/java/ca/mcgill/ecse428/restoguys/connoisseur/HistoryEspressoTest.java
+++ app/src/androidTest/java/ca/mcgill/ecse428/restoguys/connoisseur/HistoryEspressoTest.java
@@ -12,9 +12,12 @@ import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
 
 import static android.support.test.espresso.Espresso.*;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.hasToString;
+import static org.hamcrest.Matchers.startsWith;
 
 
 @RunWith(AndroidJUnit4.class)
@@ -28,8 +31,11 @@ public class HistoryEspressoTest {
     @Test
     public void checkViewsTest() {
         // Test the adapter view.
-        onData(withId(R.id.text1))
-                .check(matches(isDisplayed()));
+//        onData(withId(R.id.text1))
+//                .check(matches(isDisplayed()));
+        onData(hasToString(startsWith("Poutineville")))
+                .inAdapterView(withId(R.id.listView2)).atPosition(0)
+                .perform(click());;
 
 
     }
